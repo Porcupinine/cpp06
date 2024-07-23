7ac834bc8f57e1c75699282f67f00f648d0614a8
Laura Praca Lacerda <lpraca-l@f1r2s15.codam.nl>
2024-07-23 15:24:24 +0200

    codam

---
+++ ex00/scalarConverter.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   scalarConverter.cpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lpraca-l <lpraca-l@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/05/29 11:16:28 by lpraca-l      #+#    #+#                 */
+/*   Updated: 2024/05/29 11:16:28 by lpraca-l      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "scalarConverter.h"
+
+void scalarConverter::convert(std::string x) {
+	std::cout<<x;
+
+}
---
+++ ex00/scalarConverter.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   scalarConverter.h                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lpraca-l <lpraca-l@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/05/29 11:16:41 by lpraca-l      #+#    #+#                 */
+/*   Updated: 2024/05/29 11:16:41 by lpraca-l      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CPP06_SCALARCONVERTER_H
+#define CPP06_SCALARCONVERTER_H
+
+
+#include <string>
+
+class scalarConverter {
+	public:
+		scalarConverter() = delete;
+		~scalarConverter() = delete;
+		scalarConverter(const scalarConverter& cp) = delete;
+		scalarConverter& operator=(const scalarConverter& cp) = delete;
+		static void convert (std::string x);
+};
+
+//TODO check overflow int
+
+#endif //CPP06_SCALARCONVERTER_H
